eb29bd1beeaa82f5a26670e73437b6dd5cc4052e
Vinit Vaidya <vinit.vaidya943@gmail.com>
2026-08-11 16:02:46 +0530

    data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from cred_card_proj import logger
-from cred_card_proj.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from cred_card_proj.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from cred_card_proj.pipeline.stage_01_data_ingestion import (
+    DataIngestionTrainingPipeline,
+)
+from cred_card_proj.pipeline.stage_02_data_validation import (
+    DataValidationTrainingPipeline,
+)
+from cred_card_proj.pipeline.stage_03_data_transformation import (
+    DataTransformationTrainingPipeline,
+)
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -15,10 +22,21 @@ except Exception as e:
 
 STAGE_NAME = "Data Validation stage"
 try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_validation = DataValidationTrainingPipeline()
-   data_validation.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-   logger.exception(e)
-   raise e
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/cred_card_proj/config/configuration.py
+++ src/cred_card_proj/config/configuration.py
@@ -3,6 +3,7 @@ from cred_card_proj.utils.common import read_yaml, create_directories
 from cred_card_proj.entity.config_entity import (
     DataIngestionConfig,
     DataValidationConfig,
+    DataTransformationConfig,
 )
 
 
@@ -47,3 +48,15 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
